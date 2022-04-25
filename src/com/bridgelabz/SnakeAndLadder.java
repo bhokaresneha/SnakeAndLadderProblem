55f505716098dc4275c647c59687993af6214d4a
bhokaresneha <bhokaresneha20@gmail.com>
2022-04-25 13:12:22 +0530

    Added:single player at start position 0

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -2,6 +2,8 @@
 package com.bridgelabz;
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        System.out.println("Welcome To Snake And Ladder ");
-    }
+       // System.out.println("Welcome To Snake And Ladder ");
+                int playerPosition = 0;
+                System.out.println(+playerPosition+" " + "This is the players starting Position");
+   }
 }
