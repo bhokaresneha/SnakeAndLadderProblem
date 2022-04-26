0ee012cc643eef1c9b73bb7d4466a359297fdf40
bhokaresneha <bhokaresneha20@gmail.com>
2022-04-26 08:32:47 +0530

    Added:Exact winning position reached

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,47 +1,42 @@
 package com.bridgelabz;
-
 import java.util.Random;
 
-public class SnakeAndLadder
-{
-    static final int WINNING_POSITION=100;
-
+public class SnakeAndLadder {
+    static final int WINNING_POSITION = 100;
     public static void main(String[] args) {
         int playerPosition = 0;
+        int dies;
+        Random random =new Random();
+        System.out.println(+playerPosition+" " + "This is the players starting Position");
 
-        System.out.println(+playerPosition+" " + " is the players starting Position");
-        Random random = new Random();
-        System.out.println("roll the die to get number");
-        while(playerPosition!=WINNING_POSITION)
-        {
-            int dies = random.nextInt(6) + 1;
-            System.out.println("Number on dies =" + dies);
+        System.out.println("Rolls the dice to get number ");
+        while (playerPosition != WINNING_POSITION) {
 
-            // generating random options of ladder snake and player
-            int option=random.nextInt(3);
-
+            dies = random.nextInt(6) + 1;
+            System.out.println("dies =>"+dies);
+            int option = random.nextInt( 3);
             if (option == 0) {
-                // System.out.println("No Play");
+                System.out.println("No Play");
                 playerPosition = playerPosition;
-            }else if (option == 1)
-                {
-                    //System.out.println("Ladder");
-                    if (playerPosition>WINNING_POSITION)
-                      playerPosition = playerPosition- dies;
-                        else
-                        playerPosition = playerPosition + dies;
-
-                }
-                else {
-                         //System.out.println("Snake");
-                        playerPosition = playerPosition - dies;
-                        if (playerPosition < 0)
-                            playerPosition=0;
-
+               System.out.println("Player On::"+playerPosition);
+            }
+            else if (option == 1) {
+                System.out.println("Ladder");
+                if (playerPosition+dies>WINNING_POSITION){
+                    playerPosition = playerPosition-dies;
+                System.out.println("Player On::"+playerPosition);}
+                else{
+                    playerPosition = playerPosition + dies;
+                 System.out.println("Player On::"+playerPosition);}
+            }
+            else {
+                System.out.println("Snake");
+                playerPosition = playerPosition - dies;
+                if (playerPosition < 0)
+                    playerPosition=0;
+                System.out.println("Player On::"+playerPosition);
+            }
         }
-
-            playerPosition++;
-        }
-        System.out.println("Player Position::"+playerPosition);
     }
 }
+
